b909f918fbf2acabde3d83b22b4114cd29195425
howsem <43019679+howsem@users.noreply.github.com>
2018-11-28 15:44:43 -0800

    Add files via upload

---
+++ Exercise 3/js/Main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".summarize").click(function(){
+      $('.text').addClass('off');
+      $('.summary').addClass('on');
+      $('.loginfo').addClass('on');
+  });
+
+});
