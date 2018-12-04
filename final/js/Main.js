98880a8e1a029f484179ac5f29460648e7d3acb8
howsem <43019679+howsem@users.noreply.github.com>
2018-12-03 18:03:00 -0800

    Add files via upload

---
+++ final/js/Main.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//   $(".summarize").click(function(){
+//       $('.text').addClass('off');
+//       $('.summary').addClass('on');
+//       $('.loginfo').addClass('on');
+//       $('.info').addClass('off');
+//   });
+//
+// });
